369793a6d4e952c0ec85e64fa9642cf3e552f6e8
Kimano <Kimano117@gmail.com>
2018-02-06 01:46:17 -0500

    First Add

--- mycog.py
+++ mycog.py
@@ -1,1 +1,61 @@
+import discord
+from discord.ext import commands
+from enum import Enum
+import random
 
+class Mycog:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.heroes = buildHeroTables(self)
+
+    @commands.command(pass_context=True)
+    async def hero(self, ctx, user: discord.Member=None):
+        if(user is None):
+            user = ctx.message.author
+        name,hero = selectHero(self.heroes)
+        await self.bot.say(user.mention + " got " + name);
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
+
+def selectHero(heroes):
+    return random.choice(list(heroes.items()))
+    
+def buildHeroTables(self):
+    heroes = dict()
+    #Strength Heroes
+    heroes["Abbadon"]=Hero("Abbadon",MainStat.STRENGTH,True,True)
+    heroes["Alchemist"]=Hero("Alchemist",MainStat.STRENGTH,True,False)
+    heroes["Axe"]=Hero("Axe",MainStat.STRENGTH,True,True)
+    heroes["Beastmaster"]=Hero("Beastmaster",MainStat.STRENGTH,True,True)
+    heroes["Alchemist"]=Hero("Alchemist",MainStat.STRENGTH,True,False)
+    heroes["Axe"]=Hero("Axe",MainStat.STRENGTH,True,True)
+    #Agility Heroes
+    
+    #Intelligence Heroes
+    return heroes
+
+class Hero:
+    def __init__(self, name, mainStat, melee, hasAghs):
+        self.name = name
+        self.mainStat = mainStat
+        self.melee = melee
+        self.hasAghs = hasAghs
+        
+    def isMelee():
+        return melee
+        
+    def isRanged():
+        return not melee
+    
+class Item:
+    def __init__(self, name, cost, active):
+        self.name = name
+        self.cost = cost
+        self.active = active
+        
+class MainStat(Enum):
+    STRENGTH=1
+    AGILITY=2
+    INTELLIGENCE=3
